7acc9757d3d41e48c52f1a7ac12d5105b5b7c145
Eddie <Edwinjlau26@gmail.com>
2022-07-13 08:59:37 -0600

    validations done

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
+from wtforms.validators import (ValidationError, DataRequired)
+from datetime import datetime
 from wtforms.fields import (
     BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
 )
@@ -14,3 +15,14 @@ class AppointmentForm(FlaskForm):
     description = TextAreaField("Description", validators=[DataRequired()])
     private = BooleanField("Private?")
     submit = SubmitField('Create an appointment')
+
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+        if start >= end:
+            msg = "End date/time must come after start date/time"
+            raise ValidationError(msg)
+
+        if form.start_date.data != form.end_date.do_not_call_in_templates
+            msg = "Start/end date must be the same date"
+            raise ValidationError(msg)
